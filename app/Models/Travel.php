b3b9d6b680fe98c82ab7b595fc7930c19cc5a7ea
Nerijusanz <nerijusanz@gmail.com>
2024-07-22 14:19:17 +0300

    Create DB schema, migrations, models - close #2

---
+++ app/Models/Travel.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+use App\Models\Tour;
+use Illuminate\Support\Facades\Auth;
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Travel extends Model
+{
+    use HasFactory,Sluggable;
+
+    protected $table = 'travels';
+
+    protected $fillable = [
+        'user_id',
+        'is_public',
+        'name',
+        'slug',
+        'description',
+        'number_of_days',
+    ];
+
+    public function tours(): HasMany
+    {
+        return $this->hasMany(Tour::class);
+    }
+
+    protected static function booted()
+    {
+        static::creating(function ($model) {
+            $model->user_id = Auth::id();
+        });
+    }
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name',
+            ],
+        ];
+    }
+
+}
