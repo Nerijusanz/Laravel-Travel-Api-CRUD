5954c3f944beae606ef8e63810fca6dcd77221c8
Nerijusanz <nerijusanz@gmail.com>
2024-07-23 11:13:27 +0300

    Laravel API install close #4

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,12 +10,13 @@ use Illuminate\Notifications\Notifiable;
 use App\Models\Role;
 use App\Models\Travel;
 use App\Models\Tour;
+use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasApiTokens,HasFactory,Notifiable;
 
     /**
      * The attributes that are mass assignable.
