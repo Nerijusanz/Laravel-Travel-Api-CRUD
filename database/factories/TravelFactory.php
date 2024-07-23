ada9c9f4a9a6081f4e34c105661d07667d4fc51d
Nerijusanz <nerijusanz@gmail.com>
2024-07-23 18:59:17 +0300

    Travels tests close #15

---
+++ database/factories/TravelFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+use App\Models\User;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Travel>
+ */
+class TravelFactory extends Factory
+{
+
+    public function definition(): array
+    {
+        $title = fake()->sentence();
+        $slug = Str::slug($title, '-');
+        $numbers_of_days=rand(1,10);
+
+        return [
+            'user_id' => User::factory()->count(1)->create(),
+            'is_public' => fake()->boolean(),
+            'name' => $title,
+            'slug' => $slug,
+            'number_of_days' => $numbers_of_days,
+            'number_of_nights' => $numbers_of_days-1,
+            'description' => fake()->paragraph(),
+        ];
+    }
+}
