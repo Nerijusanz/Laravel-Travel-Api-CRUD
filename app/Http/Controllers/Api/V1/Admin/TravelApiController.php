76e9bc75aa5bd97ffc0dff5d03e503cedc35b58c
Nerijusanz <nerijusanz@gmail.com>
2024-07-26 18:43:52 +0300

    Travel Admin endpoint, route,controller,request,resource close #44

---
+++ app/Http/Controllers/Api/V1/Admin/TravelApiController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\Travel;
+use App\Http\Requests\Api\V1\Admin\TravelApiRequest;
+use App\Http\Resources\Api\V1\Admin\TravelApiResource;
+
+class TravelApiController extends Controller
+{
+
+    public function index()
+    {
+
+    }
+
+
+    public function store(TravelApiRequest $request)
+    {
+
+        $travel = Travel::create($request->validated());
+
+        return new TravelApiResource($travel);
+
+    }
+
+
+    public function show(Travel $travel)
+    {
+
+    }
+
+
+    public function update(Request $request, Travel $travel)
+    {
+
+    }
+
+
+    public function destroy(Travel $travel)
+    {
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,12 @@ use Illuminate\Support\Facades\Route;
 Route::apiResource('travels', \App\Http\Controllers\Api\V1\TravelApiController::class);
 Route::apiResource('travels/{travel}/tours', \App\Http\Controllers\Api\V1\TourApiController::class);
 
+Route::prefix('admin')->middleware([])->group(function () {
+
+    Route::apiResource('travels', \App\Http\Controllers\Api\V1\Admin\TravelApiController::class);
+
+});
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
