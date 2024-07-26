bf42d2f618578a7e114fb601d87b6e9f09c499fd
Nerijusanz <nerijusanz@gmail.com>
2024-07-26 19:52:26 +0300

    Admin Travel update #52

--- app/Http/Controllers/Api/V1/Admin/TravelApiController.php
+++ app/Http/Controllers/Api/V1/Admin/TravelApiController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers\Api\V1\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 use App\Models\Travel;
 use App\Http\Requests\Api\V1\Admin\TravelStoreApiRequest;
+use App\Http\Requests\Api\V1\Admin\TravelUpdateApiRequest;
 use App\Http\Resources\Api\V1\Admin\TravelApiResource;
 
 class TravelApiController extends Controller
@@ -34,9 +36,12 @@ class TravelApiController extends Controller
     }
 
 
-    public function update(Request $request, Travel $travel)
+    public function update(Travel $travel, TravelUpdateApiRequest $request)
     {
 
+        $travel->update($request->validated());
+
+        return (new TravelApiResource($travel))->response()->setStatusCode(Response::HTTP_ACCEPTED);
     }
 
 
---
+++ app/Http/Requests/Api/V1/Admin/TravelUpdateApiRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Requests\Api\V1\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+use Symfony\Component\HttpFoundation\Response;
+
+use App\Models\User;
+use App\Models\Travel;
+use Illuminate\Validation\Rule;
+
+class TravelUpdateApiRequest extends FormRequest
+{
+
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+
+    public function rules(): array
+    {
+        return [
+            'user_id' => ['required','integer', Rule::exists(User::class,'id')],
+            'is_public' => ['required','boolean'],
+            'name' => ['required','string','min:2','max:255', Rule::unique(Travel::class)->whereNull('deleted_at')->ignore($this->travel)],
+            'number_of_days' => ['required', 'integer'],
+            'number_of_nights' => ['required', 'integer','lt:number_of_days'],
+            'description' => ['nullable','string'],
+        ];
+    }
+
+    protected function failedValidation(Validator $validator): void
+    {
+        throw new HttpResponseException(response()->json(['errors' => $validator->errors()], Response::HTTP_UNPROCESSABLE_ENTITY));
+    }
+
+
+    public function messages(): array
+    {
+        return [
+
+        ];
+    }
+
+}
