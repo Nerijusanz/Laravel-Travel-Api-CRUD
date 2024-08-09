3419f94d472a845b9b629911e6276665474651ee
Nerijusanz <nerijusanz@gmail.com>
2024-08-09 15:03:31 +0300

    Close Model Role, scope admin, user #119

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -12,10 +12,25 @@ class Role extends Model
 {
     use HasFactory;
 
+    public const ADMIN = 'admin';
+    public const USER = 'user';
+
     protected $fillable = ['name'];
+
 
     public function users(): BelongsToMany
     {
         return $this->belongsToMany(User::class)->as('role_user');
     }
+
+    public function scopeAdmin($query)
+    {
+        return $query->where('name', self::ADMIN)->pluck('id');
+    }
+
+    public function scopeUser($query)
+    {
+        return $query->where('name', self::USER)->pluck('id');
+    }
+
 }
