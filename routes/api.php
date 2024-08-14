7f353c3c1ef0d6fc8a5f528e8d7d5b599a34ada6
Nerijusanz <nerijusanz@gmail.com>
2024-08-14 12:24:27 +0300

    Close Create Api versioning routes #131

--- routes/api.php
+++ routes/api.php
@@ -5,35 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 
 
-
-Route::apiResource('travels', \App\Http\Controllers\Api\V1\TravelApiController::class);
-Route::apiResource('travels/{travel}/tours', \App\Http\Controllers\Api\V1\TourApiController::class);
-
-
-Route::prefix('admin')->middleware(['auth:sanctum','isRole:admin'])->group(function () {
-
-    Route::apiResource('travels', \App\Http\Controllers\Api\V1\Admin\TravelApiController::class);
-    Route::apiResource('travels/{travel}/tours', \App\Http\Controllers\Api\V1\Admin\TourApiController::class);
-
-});
-
-
-Route::prefix('auth')->middleware([])->group(function () {
-
-    Route::post('login', App\Http\Controllers\Api\V1\Auth\LoginApiController::class);
-
-
-    Route::middleware(['auth:sanctum'])->group(function () {
-
-        Route::post('logout', App\Http\Controllers\Api\V1\Auth\LogoutApiController::class);
-
-    });
-
-});
-
-
-
-
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
