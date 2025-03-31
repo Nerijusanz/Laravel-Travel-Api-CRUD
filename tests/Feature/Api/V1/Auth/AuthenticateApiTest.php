b35d2e2c1b192028bde00c798e7bbbd430d3f8be
Nerijusanz <nerijusanz@gmail.com>
2025-03-31 17:15:35 +0300

    Close #202; Message text change

--- tests/Feature/Api/V1/Auth/AuthenticateApiTest.php
+++ tests/Feature/Api/V1/Auth/AuthenticateApiTest.php
@@ -46,7 +46,7 @@ class AuthenticateApiTest extends TestCase
         ]);
 
         $response->assertStatus(422);
-        $response->assertJsonFragment(['message' => 'The provided credentials are incorrect.']);
+        $response->assertJsonFragment(['message' => 'The provided credentials are incorrect']);
     }
 
     public function test_authenticate_logout_not_logged_in_user_cannot_logout_return_errors_unauthenticate_response_401(): void
