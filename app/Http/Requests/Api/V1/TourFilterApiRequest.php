7165f02b2b081f0014341f6172ffda355e5c13ae
Nerijusanz <nerijusanz@gmail.com>
2025-04-01 18:45:55 +0300

    Fix TourFilterApiRequest #216

--- app/Http/Requests/Api/V1/TourFilterApiRequest.php
+++ app/Http/Requests/Api/V1/TourFilterApiRequest.php
@@ -4,9 +4,6 @@ namespace App\Http\Requests\Api\V1;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-use Illuminate\Http\Exceptions\HttpResponseException;
-use Illuminate\Contracts\Validation\Validator;
-use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Validation\Rule;
 
 class TourFilterApiRequest extends FormRequest
@@ -22,7 +19,7 @@ class TourFilterApiRequest extends FormRequest
         return [
             'price_from' => ['nullable','numeric'],
             'price_to' => ['nullable','numeric'],
-            'date_from' => ['nullable','date'],
+            'date_from' => ['nullable', 'date'],
             'date_to' => ['nullable','date'],
             'sort_by' => ['nullable',Rule::in(['price'])],
             'order' => ['nullable',Rule::in(['asc', 'desc'])],
