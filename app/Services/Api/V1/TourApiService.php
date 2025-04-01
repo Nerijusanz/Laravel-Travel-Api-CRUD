5d51bac81b31f3b27a3e33144f91d6a2a59956c2
Nerijusanz <nerijusanz@gmail.com>
2025-04-01 17:41:44 +0300

    Fix TourApiService #214

--- app/Services/Api/V1/TourApiService.php
+++ app/Services/Api/V1/TourApiService.php
@@ -7,11 +7,11 @@ use App\Models\Travel;
 
 class TourApiService{
 
-    public function indexTour(Travel $travel, array $attributes)
+    public function indexTour(Travel $travel, array $data)
     {
         $travel->load(['tours']);
 
-        $tours = $this->getTravelToursFilterByRequest($travel,$attributes);
+        $tours = $this->getTravelToursFilterByRequest($travel, $data);
 
         return $tours;
     }
@@ -26,35 +26,34 @@ class TourApiService{
     }
 
 
-    public function getTravelToursFilterByRequest(Travel $travel, array $attributes)
+    public function getTravelToursFilterByRequest(Travel $travel, array $data)
     {
-        $tours = $travel->tours()
-            ->when(isset($attributes['price_from']), function ($query) use ($attributes) {
-                $query->where('price', '>=', $attributes['price_from'] * 100);
+        return $travel->tours()
+            ->when(isset($data['price_from']), function ($query) use ($data) {
+                $query->where('price', '>=', $data['price_from'] * 100);
             })
-            ->when(isset($attributes['price_to']), function ($query) use ($attributes) {
-                $query->where('price', '<=', $attributes['price_to'] * 100);
+            ->when(isset($data['price_to']), function ($query) use ($data) {
+                $query->where('price', '<=', $data['price_to'] * 100);
             })
-            ->when(isset($attributes['date_from']), function ($query) use ($attributes) {
-                $query->where('start_date', '>=', $attributes['date_from']);
+            ->when(isset($data['date_from']), function ($query) use ($data) {
+                $query->where('start_date', '>=', $data['date_from']);
             })
-            ->when(isset($attributes['date_to']), function ($query) use ($attributes) {
-                $query->where('start_date', '<=', $attributes['date_to']);
+            ->when(isset($data['date_to']), function ($query) use ($data) {
+                $query->where('start_date', '<=', $data['date_to']);
             })
-            ->when( (isset($attributes['sort_by']) && isset($attributes['order']) ), function ($query) use ($attributes) {
-                if (! in_array($attributes['sort_by'], ['price']) || (! in_array($attributes['order'], ['asc', 'desc']))) return;
+            ->when( (isset($data['sort_by']) && isset($data['order']) ), function ($query) use ($data) {
+                if (! in_array($data['sort_by'], ['price']) || (! in_array($data['order'], ['asc', 'desc']))) return;
 
-                $query->orderBy($attributes['sort_by'], $attributes['order']);
+                $query->orderBy($data['sort_by'], $data['order']);
             })
             ->orderBy('start_date')
             ->paginate();
 
-        return $tours;
     }
 
-    public static function formattedPrice(int|float $data)
+    public static function formattedPrice(int|float $value)
     {
-        return number_format($data, 2);
+        return number_format($value, 2);
     }
 
 }
