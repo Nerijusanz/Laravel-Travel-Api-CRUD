8fc92823dd83d6460e08cdf67a26b2c6acd72ce9
Nerijusanz <nerijusanz@gmail.com>
2025-04-03 17:47:19 +0300

    Fix Utilities Numbers generateRandomFloat #227

--- app/Utilities/Numbers.php
+++ app/Utilities/Numbers.php
@@ -5,7 +5,7 @@ namespace App\Utilities;
 class Numbers
 {
 
-    function generateRandomFloat(int|float|string $minValue, int|float|string $maxValue): float
+    public static function generateRandomFloat(int|float|string $minValue, int|float|string $maxValue): float
     {
         $minValue = ( !isset($minValue) || !is_numeric($minValue) )? 0 : $minValue;
         $maxValue = ( !isset($maxValue) || !is_numeric($maxValue) )? 0 : $maxValue;
