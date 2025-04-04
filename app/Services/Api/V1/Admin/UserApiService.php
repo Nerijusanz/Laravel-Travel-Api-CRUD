c19eb415095a553ca65eb3e4e1138112a5d6f62d
Nerijusanz <nerijusanz@gmail.com>
2025-04-04 16:56:44 +0300

    Fix AdminRoleApiMiddleware #230

--- app/Services/Api/V1/Admin/UserApiService.php
+++ app/Services/Api/V1/Admin/UserApiService.php
@@ -5,13 +5,13 @@ namespace App\Services\Api\V1\Admin;
 use App\Models\User;
 use App\Models\Role;
 
-class UserApiService{
+class UserApiService
+{
 
-    public function isAdminRole(User $user): bool
+    public static function isAdminRole(User $user): bool
     {
         return $user->roles()->where('name', Role::ADMIN)->exists();
     }
 
 }
-
 ?>
