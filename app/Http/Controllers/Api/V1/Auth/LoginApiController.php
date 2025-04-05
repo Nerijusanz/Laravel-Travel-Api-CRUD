187e46e7f37474033c6fe1b18bfe1eb1b2cf4ce4
Nerijusanz <nerijusanz@gmail.com>
2025-04-05 12:58:09 +0300

    1_Fix LoginApiController #238

--- app/Http/Controllers/Api/V1/Auth/LoginApiController.php
+++ app/Http/Controllers/Api/V1/Auth/LoginApiController.php
@@ -3,9 +3,9 @@
 namespace App\Http\Controllers\Api\V1\Auth;
 
 use App\Http\Controllers\Controller;
-
+use Illuminate\Http\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\ValidationException;
 
 use App\Models\User;
 use App\Http\Requests\Api\V1\Auth\LoginApiRequest;
@@ -14,24 +14,25 @@ use App\Http\Requests\Api\V1\Auth\LoginApiRequest;
 class LoginApiController extends Controller
 {
 
-    public function __invoke(LoginApiRequest $request)
+    public function __invoke(LoginApiRequest $request): JsonResponse
     {
+        $validated = $request->validated();
 
-        $user = User::where('email', $request->validated('email'))->first();
+        $user = User::where('email', $validated['email'])->first();
 
-        if (! $user || ! Hash::check($request->validated('password'), $user->password)) {
+        if (!$user || !Hash::check($validated['password'],$user->password)) {
 
-            throw ValidationException::withMessages([
-                'credentials' => 'The provided credentials are incorrect',
-            ]);
+            return response()->json(['errors' => 'Credentials incorrect'])->setStatusCode(Response::HTTP_UNAUTHORIZED);
         }
 
         session()->regenerate();
 
         $device = substr($request->userAgent() ?? '', 0, 255);
 
+        $token  = $user->createToken($device)->plainTextToken;
+
         return response()->json([
-            'access_token' => $user->createToken($device)->plainTextToken,
+            'access_token' => $token,
         ]);
     }
 
