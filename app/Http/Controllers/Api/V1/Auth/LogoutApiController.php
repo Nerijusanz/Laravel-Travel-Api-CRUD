cecb80a5aaa5617961af34bdf67b39e371db9603
Nerijusanz <nerijusanz@gmail.com>
2025-04-05 11:07:38 +0300

    1_Fix LogoutApiController #236

--- app/Http/Controllers/Api/V1/Auth/LogoutApiController.php
+++ app/Http/Controllers/Api/V1/Auth/LogoutApiController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers\Api\V1\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use Illuminate\Http\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class LogoutApiController extends Controller
 {
 
-    public function __invoke(Request $request)
+    public function __invoke(Request $request): JsonResponse
     {
         $request->user()->tokens()->delete();
 
