c7b303bf3ee661f22e625a03896c746e4065adea
Nerijusanz <nerijusanz@gmail.com>
2025-04-07 13:11:46 +0300

    1_Fix DatabaseSeederTest #248

--- database/seeders/tests/DatabaseSeederTest.php
+++ database/seeders/tests/DatabaseSeederTest.php
@@ -2,18 +2,19 @@
 
 namespace Database\Seeders\tests;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use Database\Seeders\RolesTableSeeder;
+use Database\Seeders\UsersTableSeeder;
 
 class DatabaseSeederTest extends Seeder
 {
 
     public function run(): void
     {
-        $this->call(UsersTableSeederTest::class);
-        $this->call(RolesTableSeederTest::class);
-        $this->call(RoleUserTableSeederTest::class);
+
+        $this->call(RolesTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
 
     }
 }
