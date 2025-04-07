31351c273d0ee08e30f4c7c7e768f61b86f8b272
Nerijusanz <nerijusanz@gmail.com>
2025-04-07 19:46:15 +0300

    4_Fix Database Migrations roles #252

--- database/migrations/2024_07_22_081914_create_roles_table.php
+++ database/migrations/2024_07_22_081914_create_roles_table.php
@@ -6,16 +6,15 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+
     public function up(): void
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->timestamps();
-            $table->softDeletes();
+
+            $table->unique(['name']);
         });
     }
 
