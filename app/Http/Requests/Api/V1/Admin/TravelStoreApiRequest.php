d6021027c2bf87009f4c7ccd6c41cd1373443d4b
Nerijusanz <nerijusanz@gmail.com>
2025-04-08 14:52:27 +0300

    1 Fix TravelStoreApiRequest #262

--- app/Http/Requests/Api/V1/Admin/TravelStoreApiRequest.php
+++ app/Http/Requests/Api/V1/Admin/TravelStoreApiRequest.php
@@ -3,14 +3,9 @@
 namespace App\Http\Requests\Api\V1\Admin;
 
 use Illuminate\Foundation\Http\FormRequest;
-
-use Illuminate\Http\Exceptions\HttpResponseException;
-use Illuminate\Contracts\Validation\Validator;
-use Symfony\Component\HttpFoundation\Response;
-
-use App\Models\User;
-use App\Models\Travel;
 use Illuminate\Validation\Rule;
+
+use App\Models\Travel;
 
 class TravelStoreApiRequest extends FormRequest
 {
@@ -26,22 +21,10 @@ class TravelStoreApiRequest extends FormRequest
         return [
             'is_public' => ['required','boolean'],
             'name' => ['required','string','min:2','max:255', Rule::unique(Travel::class)->whereNull('deleted_at')],
-            'number_of_days' => ['required', 'integer'],
+            'number_of_days' => ['required', 'integer','min:1'],
             'number_of_nights' => ['required', 'integer','lt:number_of_days'],
             'description' => ['nullable','string'],
         ];
     }
 
-    protected function failedValidation(Validator $validator): void
-    {
-        throw new HttpResponseException(response()->json(['errors' => $validator->errors()], Response::HTTP_UNPROCESSABLE_ENTITY));
-    }
-
-
-    public function messages(): array
-    {
-        return [
-
-        ];
-    }
 }
