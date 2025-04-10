dd486c7d043060b2d13803fa3cf3531e7f50afaf
Nerijusanz <nerijusanz@gmail.com>
2025-04-10 12:02:28 +0300

    TravelApiTest Validation Test #284

--- tests/Feature/Api/V1/Admin/TravelApiTest.php
+++ tests/Feature/Api/V1/Admin/TravelApiTest.php
@@ -88,6 +88,112 @@ class TravelApiTest extends TestCase
     }
 
 
+    public function test_admin_travel_api_authenticated_admin_add_travel_validation_error_response_status_422(): void
+    {
+        /*
+        php artisan test --filter=test_admin_travel_api_authenticated_admin_add_travel_validation_error_response_status_422
+        */
+
+        $this->actingAs($this->admin);
+
+        $response = $this->postJson(self::BASE_URL . '/admin/travels', [
+            'is_public' => NULL,
+            'name' => NULL,
+            'number_of_days' => NULL,
+            'number_of_nights' => NULL,
+            'description' => NULL,
+        ]);
+
+        $response->assertStatus(422);
+        $response->assertJsonStructure(['errors' => ['is_public','name','number_of_days','number_of_nights'] ]);
+
+
+        $response = $this->postJson(self::BASE_URL . '/admin/travels', [
+            'is_public' => $isPublicNotBooleanValue='x',
+            'name' => 'Travel',
+            'number_of_days' => 1,
+            'number_of_nights' => 0,
+            'description' => NULL,
+        ]);
+
+        $response->assertStatus(422);
+        $response->assertJsonStructure(['errors' => ['is_public'] ]);
+
+
+        $response = $this->postJson(self::BASE_URL . '/admin/travels', [
+            'is_public' => 1,
+            'name' => $toShortName='T',
+            'number_of_days' => 1,
+            'number_of_nights' => 0,
+            'description' => NULL,
+        ]);
+
+        $response->assertStatus(422);
+        $response->assertJsonStructure(['errors' => ['name'] ]);
+
+
+        $response = $this->postJson(self::BASE_URL . '/admin/travels', [
+            'is_public' => 1,
+            'name' => $toBigName=str()->random(256),
+            'number_of_days' => 1,
+            'number_of_nights' => 0,
+            'description' => NULL,
+        ]);
+
+        $response->assertStatus(422);
+        $response->assertJsonStructure(['errors' => ['name'] ]);
+
+
+        /****************** start name unique validation *****************/
+
+        Travel::factory()->create([
+            'is_public' => 1,
+            'name' => $uniqueName='Travel 1',
+            'number_of_days' => 1,
+            'number_of_nights' => 0,
+            'description' => NULL,
+        ]);
+
+        $response = $this->postJson(self::BASE_URL . '/admin/travels', [
+            'is_public' => 1,
+            'name' => $uniqueNameAlreadyTaken=$uniqueName,
+            'number_of_days' => 1,
+            'number_of_nights' => 0,
+            'description' => NULL,
+        ]);
+
+        $response->assertStatus(422);
+        $response->assertJsonStructure(['errors' => ['name'] ]);
+
+        /********************end name unique validation ****************/
+
+
+        $response = $this->postJson(self::BASE_URL . '/admin/travels', [
+            'is_public' => 1,
+            'name' => 'Travel',
+            'number_of_days' => $numberOfDaysIncorrectValue=0,
+            'number_of_nights' => 0,
+            'description' => NULL,
+        ]);
+
+        $response->assertStatus(422);
+        $response->assertJsonStructure(['errors' => ['number_of_days'] ]);
+
+
+        $response = $this->postJson(self::BASE_URL . '/admin/travels', [
+            'is_public' => 1,
+            'name' => 'Travel',
+            'number_of_days' => 1,
+            'number_of_nights' => $numberOfNightsIncorrectValue=1,
+            'description' => NULL,
+        ]);
+
+        $response->assertStatus(422);
+        $response->assertJsonStructure(['errors' => ['number_of_nights'] ]);
+
+    }
+
+
     public function test_admin_travel_api_authenticated_logged_in_admin_add_travel_successfully_with_valid_data(): void
     {
         /*
