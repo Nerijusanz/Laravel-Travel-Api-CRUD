a4fddc0d21f795de303597fdfb05d8cc3facf215
Nerijusanz <nerijusanz@gmail.com>
2025-04-14 18:15:06 +0300

    1_Fix_Request_Admin_TourStoreApiRequest #330

--- app/Http/Requests/Api/V1/Admin/TourStoreApiRequest.php
+++ app/Http/Requests/Api/V1/Admin/TourStoreApiRequest.php
@@ -6,7 +6,6 @@ use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
 use App\Models\Tour;
-use App\Models\Travel;
 
 class TourStoreApiRequest extends FormRequest
 {
@@ -19,8 +18,7 @@ class TourStoreApiRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'travel_id' => ['required','integer', Rule::exists(Travel::class,'id')->whereNull('deleted_at')],
-            'name' => ['required','string','min:2','max:255', Rule::unique(Tour::class)->whereNull('deleted_at')],
+            'name' => ['required','string','min:2','max:255',Rule::unique(Tour::class)->whereNull('deleted_at')],
             'price' => ['required', 'numeric','min:0'],
             'start_date' => ['required', 'date'],
             'end_date' => ['required', 'date', 'after:start_date'],
