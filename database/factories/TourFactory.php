f0495d1ba96e9f2376cf38b7391553c247df070f
Nerijusanz <nerijusanz@gmail.com>
2025-04-17 11:09:46 +0300

    2 Fix database/factories/TourFactory.php #363

--- database/factories/TourFactory.php
+++ database/factories/TourFactory.php
@@ -10,6 +10,7 @@ use App\Utilities\Numbers;
 class TourFactory extends Factory
 {
 
+    private Carbon $current;
     private string $name;
     private int|float $price;
     private string $startDate;
@@ -18,10 +19,11 @@ class TourFactory extends Factory
 
     private function loadData(): Void
     {
-        $this->name = fake()->unique()->words(3, true);
+        $this->current = Carbon::now();
+        $this->name = fake()->unique()->words(2, true);
         $this->price = Numbers::generateRandomFloat(0,1000);
-        $this->startDate = Carbon::now()->addDays(mt_rand(0,3))->startOfDay()->toDateTimeString();
-        $this->endDate = Carbon::parse($this->startDate)->addDays(mt_rand(0,3))->endOfDay()->toDateTimeString();
+        $this->startDate = ($this->current->copy())->addDays(mt_rand(0,10))->startOfDay()->toDateTimeString();
+        $this->endDate = Carbon::parse($this->startDate)->addDays(mt_rand(0,10))->endOfDay()->toDateTimeString();
     }
 
     public function definition(): array
