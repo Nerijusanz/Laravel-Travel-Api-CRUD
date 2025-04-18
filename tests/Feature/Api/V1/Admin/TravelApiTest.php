825fc43b796e46ffed53e6e5988b62e5576ff349
Nerijusanz <nerijusanz@gmail.com>
2025-04-18 15:34:13 +0300

    1 Fix tests/Feature/Api/V1/Admin/TravelApiTest.php #384

--- tests/Feature/Api/V1/Admin/TravelApiTest.php
+++ tests/Feature/Api/V1/Admin/TravelApiTest.php
@@ -88,106 +88,76 @@ class TravelApiTest extends TestCase
     }
 
 
-    public function test_admin_travel_api_authenticated_admin_add_travel_validation_error_response_status_422(): void
+    public function test_admin_travel_api_authenticated_admin_add_travel_invalid_data_return_validation_error_response_status_422(): void
     {
         /*
-        php artisan test --filter=test_admin_travel_api_authenticated_admin_add_travel_validation_error_response_status_422
+        php artisan test --filter=test_admin_travel_api_authenticated_admin_add_travel_invalid_data_return_validation_error_response_status_422
         */
 
         $this->actingAs($this->admin);
 
-        $response = $this->postJson(self::BASE_URL . '/admin/travels', [
-            'is_public' => NULL,
-            'name' => NULL,
-            'number_of_days' => NULL,
-            'number_of_nights' => NULL,
-            'description' => NULL,
-        ]);
+        $travel = Travel::factory()->create();
 
+        $endpoint = self::BASE_URL . '/admin/travels';
+
+        $invalid = [];
+        $invalid['is_public_format'] = ['is_public' => str()->random(3)];
+        $invalid['name_to_short'] = ['name' => str()->random(1)];
+        $invalid['name_to_long'] = ['name' => str()->random(256)];
+        $invalid['name_unique'] = ['name' => $travel->name];
+        $invalid['number_of_days_format'] = ['number_of_days' => str()->random(3)];
+        $invalid['number_of_days_min_1'] = ['number_of_days' => mt_rand(-5,0)];
+        $invalid['number_of_nights_format'] = ['number_of_nights' => str()->random(3)];
+        $invalid['number_of_nights_min_0'] = ['number_of_nights' => mt_rand(-100,-1)];
+        $invalid['number_of_nights_less_number_of_days'] = ['number_of_days' => $travel->number_of_days,'number_of_nights' => $travel->number_of_days];
+
+
+        $response = $this->postJson($endpoint, []);
         $response->assertStatus(422);
         $response->assertJsonStructure(['errors' => ['is_public','name','number_of_days','number_of_nights'] ]);
 
 
-        $response = $this->postJson(self::BASE_URL . '/admin/travels', [
-            'is_public' => $isPublicNotBooleanValue='x',
-            'name' => 'Travel',
-            'number_of_days' => 1,
-            'number_of_nights' => 0,
-            'description' => NULL,
-        ]);
-
+        $response = $this->postJson($endpoint, $invalid['is_public_format']);
         $response->assertStatus(422);
         $response->assertJsonStructure(['errors' => ['is_public'] ]);
 
 
-        $response = $this->postJson(self::BASE_URL . '/admin/travels', [
-            'is_public' => 1,
-            'name' => $toShortName='T',
-            'number_of_days' => 1,
-            'number_of_nights' => 0,
-            'description' => NULL,
-        ]);
-
+        $response = $this->postJson($endpoint, $invalid['name_to_short']);
         $response->assertStatus(422);
         $response->assertJsonStructure(['errors' => ['name'] ]);
 
 
-        $response = $this->postJson(self::BASE_URL . '/admin/travels', [
-            'is_public' => 1,
-            'name' => $toLongName=str()->random(256),
-            'number_of_days' => 1,
-            'number_of_nights' => 0,
-            'description' => NULL,
-        ]);
-
+        $response = $this->postJson($endpoint, $invalid['name_to_long']);
         $response->assertStatus(422);
         $response->assertJsonStructure(['errors' => ['name'] ]);
 
 
-        /****************** start name unique validation *****************/
-
-        Travel::factory()->create([
-            'is_public' => 1,
-            'name' => $uniqueName='Travel 1',
-            'number_of_days' => 1,
-            'number_of_nights' => 0,
-            'description' => NULL,
-        ]);
-
-        $response = $this->postJson(self::BASE_URL . '/admin/travels', [
-            'is_public' => 1,
-            'name' => $uniqueNameAlreadyTaken=$uniqueName,
-            'number_of_days' => 1,
-            'number_of_nights' => 0,
-            'description' => NULL,
-        ]);
-
+        $response = $this->postJson($endpoint, $invalid['name_unique']);
         $response->assertStatus(422);
         $response->assertJsonStructure(['errors' => ['name'] ]);
 
-        /********************end name unique validation ****************/
 
-
-        $response = $this->postJson(self::BASE_URL . '/admin/travels', [
-            'is_public' => 1,
-            'name' => 'Travel',
-            'number_of_days' => $numberOfDaysIncorrectValue=0,
-            'number_of_nights' => 0,
-            'description' => NULL,
-        ]);
-
+        $response = $this->postJson($endpoint, $invalid['number_of_days_format']);
         $response->assertStatus(422);
         $response->assertJsonStructure(['errors' => ['number_of_days'] ]);
 
 
-        $response = $this->postJson(self::BASE_URL . '/admin/travels', [
-            'is_public' => 1,
-            'name' => 'Travel',
-            'number_of_days' => 1,
-            'number_of_nights' => $numberOfNightsIncorrectValue=1,
-            'description' => NULL,
-        ]);
+        $response = $this->postJson($endpoint, $invalid['number_of_days_min_1']);
+        $response->assertStatus(422);
+        $response->assertJsonStructure(['errors' => ['number_of_days'] ]);
 
+
+        $response = $this->postJson($endpoint, $invalid['number_of_nights_format']);
+        $response->assertStatus(422);
+        $response->assertJsonStructure(['errors' => ['number_of_nights'] ]);
+
+
+        $response = $this->postJson($endpoint, $invalid['number_of_nights_min_0']);
+        $response->assertStatus(422);
+        $response->assertJsonStructure(['errors' => ['number_of_nights'] ]);
+
+
+        $response = $this->postJson($endpoint, $invalid['number_of_nights_less_number_of_days']);
         $response->assertStatus(422);
         $response->assertJsonStructure(['errors' => ['number_of_nights'] ]);
 
