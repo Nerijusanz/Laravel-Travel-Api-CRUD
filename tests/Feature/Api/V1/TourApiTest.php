31ebda03968fee76c18f5ad65f5c02f18e1a59c9
Nerijusanz <nerijusanz@gmail.com>
2025-04-23 18:43:24 +0300

    10 Fix tests/Feature/Api/V1/TourApiTest.php #402

--- tests/Feature/Api/V1/TourApiTest.php
+++ tests/Feature/Api/V1/TourApiTest.php
@@ -116,7 +116,7 @@ class TourApiTest extends TestCase
 
         $this->actingAs($this->admin);
 
-        $travel = Travel::factory(['is_public' => true])->create();
+        $travel = Travel::factory(['is_public' => 1])->create();
 
         $tourLater = Tour::factory(['travel_id' => $travel->id])->create();
 
