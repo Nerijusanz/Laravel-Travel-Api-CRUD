145a2187ccc6c5001a708bf01a7c3df06f626f0a
Nerijusanz <nerijusanz@gmail.com>
2025-04-23 17:33:23 +0300

    3 Fix tests/Feature/Api/V1/TourApiTest.php #402

--- tests/Feature/Api/V1/TourApiTest.php
+++ tests/Feature/Api/V1/TourApiTest.php
@@ -152,8 +152,8 @@ class TourApiTest extends TestCase
         $tourCheapEarlier = Tour::factory([
             'travel_id' => $travel->id,
             'price' => $tourCheapLater->price,
-            'start_date' => $startDate = Carbon::parse($tourCheapLater->start_date)->subDays(1)->startOfDay()->toDateTimeString(),
-            'end_date' => Carbon::parse($startDate)->addDays(0)->endOfDay()->toDateTimeString(),
+            'start_date' => $startDate = Carbon::parse($tourCheapLater->start_date)->subDays(mt_rand(1,10))->startOfDay()->toDateTimeString(),
+            'end_date' => Carbon::parse($startDate)->addDays(mt_rand(0,10))->endOfDay()->toDateTimeString(),
             ])->create();
 
         $tourExpensive = Tour::factory([
