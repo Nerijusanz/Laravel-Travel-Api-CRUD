97d96742ab3c3a11151feb260e765d7a7b150304
Nerijusanz <nerijusanz@gmail.com>
2025-04-24 11:33:06 +0300

    1 Fix database/factories/TourFactory.php #404

--- database/factories/TourFactory.php
+++ database/factories/TourFactory.php
@@ -3,14 +3,14 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-
 use Carbon\Carbon;
+
 use App\Utilities\Numbers;
 
 class TourFactory extends Factory
 {
 
-    private Carbon $current;
+    private Carbon $dateTime;
     private string $name;
     private int|float $price;
     private string $startDate;
@@ -19,10 +19,10 @@ class TourFactory extends Factory
 
     private function loadData(): Void
     {
-        $this->current = Carbon::now();
-        $this->name = fake()->unique()->words(2, true);
-        $this->price = Numbers::generateRandomFloat(0,1000);
-        $this->startDate = ($this->current->copy())->addDays(mt_rand(0,10))->startOfDay()->toDateTimeString();
+        $this->dateTime = Carbon::now();
+        $this->name = fake()->unique()->words(mt_rand(1,3), true);
+        $this->price = Numbers::generateRandomFloat(0,2000);
+        $this->startDate = $this->dateTime->addDays(mt_rand(0,10))->startOfDay()->toDateTimeString();
         $this->endDate = Carbon::parse($this->startDate)->addDays(mt_rand(0,10))->endOfDay()->toDateTimeString();
     }
 
