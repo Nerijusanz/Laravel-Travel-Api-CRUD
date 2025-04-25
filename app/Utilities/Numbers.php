ecce85375949ff00ca558a65dabb507aa5519a76
Nerijusanz <nerijusanz@gmail.com>
2025-04-25 13:27:15 +0300

    3 Fix app/Utilities/Numbers.php #414

--- app/Utilities/Numbers.php
+++ app/Utilities/Numbers.php
@@ -7,14 +7,12 @@ class Numbers
 
     public static function generateRandomFloat(int|float|string $minValue, int|float|string $maxValue): float
     {
-
         $minValue = ( !isset($minValue) || !is_numeric($minValue) )? 0 : $minValue;
         $maxValue = ( !isset($maxValue) || !is_numeric($maxValue) )? 0 : $maxValue;
 
         $value =  $minValue + mt_rand() / mt_getrandmax() * ($maxValue - $minValue);
 
-        return self::number_format_decimal($value);
-
+        return Numbers::number_format_decimal($value);
     }
 
     public static function number_format_decimal(int|float|string $value): float
