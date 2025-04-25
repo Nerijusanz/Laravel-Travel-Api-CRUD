b0513b9f56a0c426a76f0738dcfe6eead5fa5257
Nerijusanz <nerijusanz@gmail.com>
2025-04-25 14:45:50 +0300

    1 Fix app/Utilities/Numbers.php #416

--- app/Utilities/Numbers.php
+++ app/Utilities/Numbers.php
@@ -4,8 +4,7 @@ namespace App\Utilities;
 
 class Numbers
 {
-
-    public static function generateRandomFloat(int|float|string $minValue, int|float|string $maxValue): float
+    public static function generateRandomFloat(int|float|string $minValue, int|float|string $maxValue)
     {
         $minValue = ( !isset($minValue) || !is_numeric($minValue) )? 0 : $minValue;
         $maxValue = ( !isset($maxValue) || !is_numeric($maxValue) )? 0 : $maxValue;
@@ -15,28 +14,24 @@ class Numbers
         return Numbers::number_format_decimal($value);
     }
 
-    public static function number_format_decimal(int|float|string $value): float
+    public static function number_format_decimal(int|float|string $value)
     {
         return number_format(
-                        floatval($value),
+                        $value,
                         config('app.settings.decimal'),
                         config('app.settings.decimal_separator'),
                         config('app.settings.thousands_separator')
                     );
     }
 
-    public static function getAttributeNumberValue(int $value): float
+    public static function getAttributeNumberValue(int $value)
     {
-        $value = ($value / 100);
-
-        return Numbers::number_format_decimal($value);
+        return Numbers::number_format_decimal( ($value / 100) );
     }
 
-    public static function setAttributeNumberValue(int|float|string $value): int
+    public static function setAttributeNumberValue(int|float|string $value)
     {
-        if( !isset($value) || !is_numeric($value) ) return 0;
-
-        return (int)($value * 100);
+        return ( !isset($value) || !is_numeric($value) ) ? 0 : ($value * 100);
     }
 
 }
