8bc95520f912b83cc97349297e20b6fe25825bce
Nerijusanz <nerijusanz@gmail.com>
2025-04-28 17:51:02 +0300

    5 Fix tests/Feature/Api/V1/TourApiTest.php #438

--- tests/Feature/Api/V1/TourApiTest.php
+++ tests/Feature/Api/V1/TourApiTest.php
@@ -213,13 +213,18 @@ class TourApiTest extends TestCase
 
         $this->assertCount(1, $travel->tours()->get());
 
-        $tour = $travel->tours()->first();
+        $this->actingAs($this->user);
 
         $endpoint = self::BASE_URL . '/travels/'. $travel->id .'/tours';
 
+        $tour = $travel->tours()->findOrFail($tour->id);
+
         $response = $this->getJson($endpoint);
         $response->assertStatus(200);
-        $response->assertJsonFragment(['price' => $tour->price]);
+        $response->assertJsonFragment([
+                                'id' => $tour->id,
+                                'price' => $tour->price
+                            ]);
 
     }
 
