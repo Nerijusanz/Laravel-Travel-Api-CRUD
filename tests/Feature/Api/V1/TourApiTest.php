d6a94d3865e350ea7f04640cb78d16f988d09227
Nerijusanz <nerijusanz@gmail.com>
2025-04-29 11:41:08 +0300

    6 Fix tests/Feature/Api/V1/TourApiTest.php #441

--- tests/Feature/Api/V1/TourApiTest.php
+++ tests/Feature/Api/V1/TourApiTest.php
@@ -327,10 +327,12 @@ class TourApiTest extends TestCase
                                     'travel_id' => $travel->id,
                                     'price' => $tourExpensiveLater->price,
                                     'start_date' => $startDate = Carbon::parse($tourExpensiveLater->start_date)->subDays(mt_rand(1,10))->startOfDay()->toDateTimeString(),
-                                    'end_date' => $endDate = Carbon::parse($startDate)->addDays(mt_rand(1,10))->endOfDay()->toDateTimeString(),
+                                    'end_date' => $endDate = Carbon::parse($startDate)->addDays(mt_rand(0,10))->endOfDay()->toDateTimeString(),
                                     ])->create();
 
         $this->assertCount(3, $travel->tours()->get());
+
+        $this->actingAs($this->user);
 
         $endpoint = self::BASE_URL . '/travels/'. $travel->id .'/tours?sort_by=price&order=desc';
 
